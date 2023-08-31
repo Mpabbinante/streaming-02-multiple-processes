e8513d0cf9ba7c6944cef058bf48ce9a132757f1
mpabbinante <mpabbinante@gmail.com>
2023-08-31 10:49:17 -0500

    Fixed to create correct output files

--- process_streaming_output99.py
+++ process_streaming_output99.py
@@ -18,82 +18,75 @@ https://wiki.python.org/moin/UdpCommunication
 """
 
 # Import from Python Standard Library
-
 import csv
 import socket
 import time
 import logging
 
 # Set up basic configuration for logging
-
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-
-# Declare program constants (typically constants are named with ALL_CAPS)
-
+# Constants
 HOST = "localhost"
 PORT = 9999
 ADDRESS_TUPLE = (HOST, PORT)
 INPUT_FILE_NAME = "us-counties.csv"
 OUTPUT_FILE_NAME = "out99.txt"
-# Define program functions (bits of reusable code)
 
-
+# Prepare message from a row of data
 def prepare_message_from_row(row):
-    """Prepare a binary message from a given row."""
     date, county, state, fips, cases, death = row
-    # use an fstring to create a message from our data
-    # notice the f before the opening quote for our string?
     fstring_message = f"[{date}, {county}, {state}, {fips}, {cases}, {death}]"
-
-    # prepare a binary (1s and 0s) message to stream
     MESSAGE = fstring_message.encode()
     logging.debug(f"Prepared message: {fstring_message}")
     return MESSAGE
 
+# Stream and write data
+def stream_row(input_file_name, address_tuple, num_rows=9):
+    logging.info(f"Starting to stream and write {num_rows} rows of data from {input_file_name} to {address_tuple}.")
 
-def stream_row(input_file_name, address_tuple):
-    """Read from input file and stream data."""
-    logging.info(f"Starting to stream data from {input_file_name} to {address_tuple}.")
+    sock_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Create a file object for input (r = read access)
+    # Open input file
     with open(input_file_name, "r") as input_file:
         logging.info(f"Opened for reading: {input_file_name}.")
-
-        # Create a CSV reader object
         reader = csv.reader(input_file, delimiter=",")
-        
-        header = next(reader)  # Skip header row
+        header = next(reader)
         logging.info(f"Skipped header row: {header}")
 
-        # use socket enumerated types to configure our socket object
-        # Set our address family to (IPV4) for 'internet'
-        # Set our socket type to UDP (datagram)
-        ADDRESS_FAMILY = socket.AF_INET 
-        SOCKET_TYPE = socket.SOCK_DGRAM 
+        # Open output file
+        with open(OUTPUT_FILE_NAME, "w", newline="") as output_file:
+            logging.info(f"Opened for writing: {OUTPUT_FILE_NAME}.")
+            writer = csv.writer(output_file, delimiter=",")
 
-        # Call the socket constructor, socket.socket()
-        # A constructor is a special method with the same name as the class
-        # Use the constructor to make a socket object
-        # and assign it to a variable named `sock_object`
-        sock_object = socket.socket(ADDRESS_FAMILY, SOCKET_TYPE)
-        
-        for row in reader:
-            MESSAGE = prepare_message_from_row(row)
-            sock_object.sendto(MESSAGE, address_tuple)
-            logging.info(f"Sent: {MESSAGE} on port {PORT}. Hit CTRL-c to stop.")
-            time.sleep(3) # wait 3 seconds between messages
-            
+            # Write the header row
+            writer.writerow(header)
+
+            # Stream and write each row
+            for _ in range(num_rows):
+                row = next(reader, None)
+                if row is None:
+                    break
+
+                # Prepare message and send via UDP
+                MESSAGE = prepare_message_from_row(row)
+                sock_object.sendto(MESSAGE, address_tuple)
+                logging.info(f"Sent: {MESSAGE} on port {PORT}. Hit CTRL-c to stop.")
+                
+                # Write the row to the output file
+                writer.writerow(row)
+                
+                # Wait before sending the next message
+                time.sleep(3)
 
 if __name__ == "__main__":
     try:
         logging.info("===============================================")
         logging.info("Starting fake streaming process.")
-        stream_row(INPUT_FILE_NAME, ADDRESS_TUPLE)
+        stream_row(INPUT_FILE_NAME, ADDRESS_TUPLE, num_rows=99)
         logging.info("Streaming complete!")
         logging.info("===============================================")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
-
